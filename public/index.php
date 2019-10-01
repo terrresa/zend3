c9180067e72fc310973a62767dfc12e8916eb2e7
eleana <eleana_marchan@hotmail.com>
2019-09-30 23:26:02 -0500

    tag

--- public/index.php
+++ public/index.php
@@ -8,7 +8,7 @@ use Zend\Stdlib\ArrayUtils;
  * to the application root now.
  */
 chdir(dirname(__DIR__));
-<?php echo demo_tag;?>
+<?php echo demo_tag12;?>
 
 // Decline static file requests back to the PHP built-in webserver
 if (php_sapi_name() === 'cli-server') {
